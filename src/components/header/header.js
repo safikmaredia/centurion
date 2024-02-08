525fd6a265ced1f9eb1ea38beead01d07d75d99f
Your Name <youremail@example.com>
2024-02-07 22:27:36 -0500

    header coded and styled

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './header.scss';
+
+const Header = () => {
+    return (
+        <p className="header_background">Free shipping in Ontario on all orders over $60</p>
+    )
+
+};
+
+export default Header;
